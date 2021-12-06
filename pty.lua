f5af7f0c7b484b25284f24f085ceb864aeba3e7b
aitap <krylov.r00t@gmail.com>
2021-12-06 20:35:43 +0000

    port the experiment to luaposix

---
+++ pty.lua
@@ -0,0 +1,85 @@
+#!/usr/bin/lua5.3
+local P = require'posix'
+
+-- doesn't have to send all input, will be called again by the poll loop
+local function pump(from, to)
+	local buf = assert(P.read(from, 1024))
+	local written = 0
+	while written < #buf do
+		written = written + assert(P.write(to, buf:sub(written+1)))
+	end
+end
+
+-- return master PTY file descriptor, but slave PTY path
+local function make_pty()
+	local ptm = P.openpt(P.O_RDWR|P.O_NOCTTY)
+	assert(P.grantpt(ptm))
+	assert(P.unlockpt(ptm))
+	local ptsp = assert(P.ptsname(ptm))
+	return ptm, ptsp
+end
+
+-- exec child process with stdio connected to a given TTY
+local function child_exec(ptsp, path, args)
+	assert(P.setpid('s'))
+	-- will set the controlling TTY
+	local pts = assert(P.open(ptsp, P.O_RDWR))
+	assert(P.dup2(pts, P.STDIN_FILENO))
+	assert(P.dup2(pts, P.STDOUT_FILENO))
+	assert(P.dup2(pts, P.STDERR_FILENO))
+	assert(P.execp(path, args))
+end
+
+local function clone(x)
+	local cpy = {}
+	for k,v in pairs(x) do cpy[k] = v end
+	return cpy
+end
+
+local function cfmakeraw(att)
+	att.iflag = att.iflag & ~(
+		P.IGNBRK | P.BRKINT | P.PARMRK | P.ISTRIP | P.INLCR | P.IGNCR
+		| P.ICRNL | P.IXON
+	)
+	att.oflag = att.oflag & ~P.OPOST
+	att.lflag = att.lflag & ~(
+		P.ECHO | P.ECHONL | P.ICANON | P.ISIG | P.IEXTEN
+	)
+	att.cflag = att.cflag & ~(P.CSIZE | P.PARENB)
+	att.cflag = att.cflag | P.CS8
+end
+
+-- run fn() when garbage-collecting the return value
+local function guard(fn)
+	local ret = {__gc = fn}
+	return setmetatable(ret, ret)
+end
+
+-- pump raw mode stdin to ptm and ptm to stdout
+local function parent_loop(ptm)
+	local att = assert(P.tcgetattr(P.STDIN_FILENO))
+	-- restore terminal on shutdown
+	local oatt = clone(att)
+	local atexit = guard(function()
+		assert(P.tcsetattr(P.STDIN_FILENO, P.TCSANOW, oatt))
+	end)
+	cfmakeraw(att)
+	assert(P.tcsetattr(P.STDIN_FILENO, P.TCSANOW, att))
+
+	local fds = {
+		[P.STDIN_FILENO] = { events = { IN = true } },
+		[      ptm     ] = { events = { IN = true } },
+	}
+	while P.poll(fds) do
+		if fds[P.STDIN_FILENO].revents.IN then pump(P.STDIN_FILENO, ptm)
+		elseif fds[ptm].revents.IN then pump(ptm, P.STDOUT_FILENO)
+		else break end
+	end
+end
+
+local ptm, ptsp = make_pty()
+local pid = assert(P.fork())
+if pid > 0 then parent_loop(ptm) else
+	assert(P.close(ptm))
+	child_exec(ptsp, 'R', {})
+end
